5b6967816ce0d4ea94a87e694b163c40233d6b89
Mayur Bhil <mayurbhil47@gmail.com>
2024-12-13 16:43:52 +0530

    get Apis

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ const port  = 3000;
 const User = require("./models/user.js")
 
 app.use(express.json())
+
 app.post("/signup",async (req,res)=>{
     //createing a new Instance OF a model and Sending The DUmmy Data
-    console.log(req.body);
     
     const user = new User(req.body);
    try{
@@ -16,6 +16,40 @@ app.post("/signup",async (req,res)=>{
    }catch(err){
         res.status(400).json("Error User",err)
    }
+})
+
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.emailId;
+   try{
+   const users = await User.find({emailId: userEmail});
+   if(users.length ==0){
+        res.status(404).send("USer not Found ..!")
+   }else{
+        res.send(users);
+   }
+   
+   }catch(err){
+    res.status(400).send("Somthing Went Wrong..!")
+   }
+})
+
+app.get("/feed",async(req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users)
+        }catch(err){
+         res.status(400).send("Somthing Went Wrong..!")
+        }
+})
+
+app.get("/findone",async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.findOne({emailId:userEmail});
+        res.send(users)
+        }catch(err){
+         res.status(404).send("User Not Found ..!")
+        }
 })
 
 connectDB()
